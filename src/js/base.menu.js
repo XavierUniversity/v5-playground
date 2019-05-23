389a47f439fdb417b772dcd1500cc6ea3a9d0410
Greg McMullen <mcmullen.greg@gmail.com>
2019-05-23 11:42:06 -0400

    Driving myself crazy, Miss Daisy

--- src/js/base.menu.js
+++ src/js/base.menu.js
@@ -3,62 +3,113 @@ var focusableElementsString = "a[href], area[href], input:not([disabled]), selec
 
 // store the item that has focus before opening the modal window
 var focusedElementBeforeModal;
-var state = 'closed';
-function setFocusToFirstItemInModal(obj){
+var id;
+jQuery('.drawer-open').click(function(e){
+  e.preventDefault();
+  id = $(this).data("controls");
+  openDrawer($('#'+id));
+});
+
+jQuery('.drawer-close').click(function(e){
+  closeDrawer();
+});
+
+jQuery('.drawer').keydown(function(e){
+  trapEscapeKey($(this), e);
+});
+
+jQuery('.drawer').keydown(function(e){
+  trapTabKey($(this), e);
+});
+  
+jQuery("#overlay").click(function(e){
+  closeDrawer();
+});
+
+
+function trapEscapeKey(obj, e){
+  if ( e.which == 27){
+    var o = obj.find('*');
+    var cancelElement = o.filter(".drawer-close");
+    cancelElement.click();
+    e.preventDefault();
+  }
+}
+
+function trapTabKey(obj, e){
+  // if tab or shift-tab pressed
+  if (e.which == 9) {
+    // get list of all children elements in given object
+    var o = obj.find('*');
+    
+    // get list of focusable items
+    var focusableItems;
+    focusableItems = o.filter(focusableElementsString).filter(':visible');
+    
+    // get currently focused item
+    var focusedItem;
+    focusedItem = jQuery(':focus');
+    
+    // get the number of focusable items
+    var numberOfFocusableItems;
+    numberOfFocusableItems = focusableItems.length;
+    
+    // get the index of the currently focused item
+    var focusedItemIndex;
+    focusedItemIndex = focusableItems.index(focusedItem);
+  
+    if (e.shiftKey) {
+      //back tab
+      // if focused on first item and user preses back-tab, go to the last focusable item
+      if (focusedItemIndex === 0) {
+        focusableItems.get(numberOfFocusableItems - 1).focus();
+        e.preventDefault();
+      }
+
+    } else {
+      //forward tab
+      // if focused on the last item and user preses tab, go to the first focusable item
+      if (focusedItemIndex == numberOfFocusableItems - 1) {
+        focusableItems.get(0).focus();
+        e.preventDefault();
+      }
+    }
+  }
+}
+
+function setFocusToFirstItem(obj) {
   // get list of all children elements in given object
   var o = obj.find('*');
 
-  // set the focus to the first keyboard focusable item
+  // set focus to first focusable item
   o.filter(focusableElementsString).filter(':visible').first().focus();
 }
 
-function toggleDrawer(target){
-  // toggle the visibility of the nav and overlay
-  $(target.data.t).toggleClass("visible");
-  if ( target.data.t == "#main-nav" ){
-    $("body").toggleClass("nav-open");  
+function openDrawer(obj){
+  if ( obj.attr("id") == "main-nav"){ // Change the body class for main nav
+    jQuery("body").addClass('nav-open'); // will show the overlay
   }
-  // Set the aria attributes for showing/hiding appropriate content
-  $(target.data.t).attr('aria-hidden', $(target.data.t).attr('aria-hidden') == 'false' ? 'true' : 'false');
-  $("#content").attr('aria-hidden', $(target.data.t).attr('aria-hidden') == 'false' ? 'true' : 'false');
+  jQuery(obj).addClass('visible'); // Make the drawer visible
+  jQuery(obj).attr('aria-hidden', false); // mark it as no longer hidden
+  jQuery("#content").attr('aria-hidden', true); // mark content as hidden
   
-  // attach an event listener to redirect the tab to the modal window.
-  $('body').on('focusin', '#content', function(){
-    setFocusToFirstItemInModal($(target.data.t));
+  // listener to redirect tab to drawer if they somehow get out...
+  jQuery('body').on('focusin', '#content', function(){
+    setFocusToFirstItem(jQuery(obj));
   });
-  
+  // save current focus
   focusedElementBeforeModal = jQuery(':focus');
-  if ( state == 'closed' ){
-    setFocusToFirstItemInModal($("target")); 
-    state = 'opened';
-  } else{
-    focusedElementBeforeModal.focus();
-    state = 'closed';
-  }
-  return false;
+
+  setFocusToFirstItem(obj);
 }
 
-function trapEscapeKey(obj, evt) {
-  // if escape pressed
-  if (evt.which == 27) {
-    // get list of all children elements in given object
-    var o = obj.find('*');
-    // get list of focusable items
-    
-    var cancelElement;
-    cancelElement = o.filter(['.menu_toggle', '.search_toggle']);
-
-    // close the modal window
-    cancelElement.click();
-    evt.preventDefault();
-  }
-  return false;
+function closeDrawer(){
+  jQuery("body").removeClass('nav-open'); // remove the overlay
+  jQuery('.drawer').removeClass('visible'); // remove drawer from visible flow
+  jQuery('.drawer').attr('aria-hidden', true); // hide drawer
+  jQuery("#content").attr('aria-hidden', false); // make content visible again
+  // Remove listener
+  jQuery('body').off('focusin', "#content");
+  // set focus back to element that had focus before modal.
+  focusedElementBeforeModal.focus();
 }
-
-// Click events...
-$(".menu_toggle, #overlay").on("click", { t: "#main-nav" }, toggleDrawer);
-$(".search_toggle").on("click", { t: "#search" }, toggleDrawer);
-// Trap the escape key...
-$('#main-nav, #search').keydown(function(event) {
-  trapEscapeKey($(this), event);
-});

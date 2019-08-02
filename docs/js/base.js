31cdc8cd6453933c7f57868572a47bb921e9935d
Greg McMullen <mcmullen.greg@gmail.com>
2019-08-02 09:54:59 -0400

    Update to the homepage

--- docs/js/base.js
+++ docs/js/base.js
@@ -1,3 +1,12 @@
+var insta = $("[data-image]").data("image");
+$.get("https://api.instagram.com/oembed?url=" + insta + "&hidecaption=true&omitscript=true", function(d){
+  var parent = $("[data-image='"+ insta +"']");
+  console.log(parent);
+  parent.css({"background-image": 'url("' +d.thumbnail_url + '")', "background-size": 'cover'});
+  parent.find(".feature__content").attr('href', insta);
+  parent.find(".feature__name").html('<span class="feature__link">'+ d.author_name + '</span>');
+  parent.find(".feature__caption").html(d.title);
+});
 // jQuery formatted selector to search for focusable items
 var focusableElementsString = "a[href], area[href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), iframe, object, embed, *[tabindex], *[contenteditable]";
 

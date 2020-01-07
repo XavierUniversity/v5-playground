5bdae1c9cc4d266ecc187d6bcf6efc827fcd578e
Greg McMullen <mcmullen.greg@gmail.com>
2020-01-07 10:43:27 -0500

    Updates to the insta pull

--- docs/js/base.js
+++ docs/js/base.js
@@ -102,14 +102,11 @@ $("#all-g-button").click(function() {
 var insta = $("[data-image]").data("image");
 $.get("https://api.instagram.com/oembed?url=" + insta + "&hidecaption=true&omitscript=true", function(d){
   var parent = $("[data-image='"+ insta +"']");
-  console.log(parent);
-  parent.css({"background-image": 'url("' +d.thumbnail_url + '")', "background-size": 'cover'});
+  parent.css({"background-image": 'url("' + insta + 'media?size=l")', "background-size": 'cover'});
   parent.find(".news--feature__content").attr('href', insta);
   parent.find(".news--feature__name").html('<span class="news--feature__link">'+ '<img src="images/news/instagram.png" style="padding-right:.3rem;">' + d.author_name + '</span>');
   parent.find(".news--feature__caption").html(d.title);
 });
-
-
 // jQuery formatted selector to search for focusable items
 var focusableElementsString = "a[href], area[href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), iframe, object, embed, *[tabindex], *[contenteditable]";
 

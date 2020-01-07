5a6d11ff3ec5ed0863243474bfc8b33390b87952
Gregory McMullen <mcmullengreg@users.noreply.github.com>
2020-01-07 10:44:13 -0500

    Updates to the insta pull (#107)

--- src/js/base.instagram.js
+++ src/js/base.instagram.js
@@ -1,10 +1,8 @@
 var insta = $("[data-image]").data("image");
 $.get("https://api.instagram.com/oembed?url=" + insta + "&hidecaption=true&omitscript=true", function(d){
   var parent = $("[data-image='"+ insta +"']");
-  console.log(parent);
-  parent.css({"background-image": 'url("' +d.thumbnail_url + '")', "background-size": 'cover'});
+  parent.css({"background-image": 'url("' + insta + 'media?size=l")', "background-size": 'cover'});
   parent.find(".news--feature__content").attr('href', insta);
   parent.find(".news--feature__name").html('<span class="news--feature__link">'+ '<img src="images/news/instagram.png" style="padding-right:.3rem;">' + d.author_name + '</span>');
   parent.find(".news--feature__caption").html(d.title);
 });
-

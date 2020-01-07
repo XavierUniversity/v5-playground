46fd2edce82e12d5b5df2e6a2c68e46d2a5ba6a3
Greg McMullen <mcmullen.greg@gmail.com>
2020-01-07 12:20:22 -0500

    Update the insta icon

--- docs/js/base.js
+++ docs/js/base.js
@@ -104,7 +104,7 @@ $.get("https://api.instagram.com/oembed?url=" + insta + "&hidecaption=true&omits
   var parent = $("[data-image='"+ insta +"']");
   parent.css({"background-image": 'url("' + insta + 'media?size=l")', "background-size": 'cover'});
   parent.find(".news--feature__content").attr('href', insta);
-  parent.find(".news--feature__name").html('<span class="news--feature__link">'+ '<img src="images/news/instagram.png" style="padding-right:.3rem;">' + d.author_name + '</span>');
+  parent.find(".news--feature__name").html('<span class="news--feature__link">'+ '<svg height="45" width="46"><use xlink:href="#xu-instagram"></use></svg>' + d.author_name + '</span>');
   parent.find(".news--feature__caption").html(d.title);
 });
 // jQuery formatted selector to search for focusable items

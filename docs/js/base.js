491992f2abcc097a806e3d7ed6b06d7ee1db1c92
Greg McMullen <mcmullen.greg@gmail.com>
2020-01-07 20:51:12 -0500

    Add editor link from meta ID

--- docs/js/base.js
+++ docs/js/base.js
@@ -1,3 +1,6 @@
+if ( $('meta[property="id"]').attr('content') !== undefined ){
+  $("#editorAccess").attr('href', "https://cascade.xavier.edu/entity/open.act?id="+ $('meta[property="id"]').attr('content') +"&type=page");
+}
 $("#all-button").click(function() {
      $(".select-box__container").addClass("academic-programs__visible");
      $("h2").show();

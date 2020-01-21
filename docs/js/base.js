7e9040c054804002e792ff04182bd85b16cb5452
Greg McMullen <mcmullen.greg@gmail.com>
2020-01-21 10:05:19 -0500

    Compile JS

--- docs/js/base.js
+++ docs/js/base.js
@@ -28,7 +28,7 @@ if ( instas.length > 0 ){
       parent.css({"background-image": 'url("' + insta + 'media?size=l")', "background-size": 'cover'});
       parent.find(".news--feature__content").attr('href', insta);
       parent.find(".news--feature__name").html('<span class="news--feature__link">'+ '<svg height="45" width="46"><use xlink:href="#xu-instagram"></use></svg>' + d.author_name + '</span>');
-      parent.find(".news--feature__caption").html(d.title);
+      parent.find(".news--feature__caption").html(d.title.slice(0, 100) + ( d.title.length > 100 ? "&hellip;" : ''));
     });
   }
 }

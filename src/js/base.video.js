37d4f2d9f782429423dbb13e42eec05f41b5e9e2
Greg McMullen <mcmullen.greg@gmail.com>
2020-10-22 13:45:46 -0400

    Remove the dependency of the plyr script for simple video modal

--- src/js/base.video.js
+++ src/js/base.video.js
@@ -5,3 +5,13 @@ if ( !detectmob() ){
     vid.html('<source src="'+vid.data("bgvideo") + '" type="video/mp4" />');
   }
 }
+// Video Button -- for things like Campus Life / Diversity
+$("#vid-btn").click(function(){
+    $("#video-overlay iframe").attr('src', $(this).data('video'));
+    $("#video-overlay").show();
+});
+// Video Overlay
+$("#close-video-overlay").click(function(){
+    $("#video-overlay").hide();
+    $("#video-overlay iframe").attr('src','');  
+});

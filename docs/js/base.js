110cbc04e055e2ea08fcd517bd57faac781c1dd1
Greg McMullen <mcmullen.greg@gmail.com>
2020-10-29 14:54:21 -0400

    Instagram update

--- docs/js/base.js
+++ docs/js/base.js
@@ -116,12 +116,15 @@ var instas = $("[data-image]");
 if ( instas.length > 0 ){
   for ( var i = 0; i < instas.length; i++){
     var insta = $(instas[i]).data('image');
-    $.get("https://api.instagram.com/oembed?url=" + insta + "&hidecaption=true&omitscript=true", function(d){
+    $.ajax({
+      dataType: "json",
+      url: "https://www.xavier.edu/_V5/api/insta.php",
+      data: { instagram_post : insta }
+    }).done(function(d){
       var parent = $("[data-image='"+ insta +"']");
-      parent.css({"background-image": 'url("' + insta + 'media?size=l")', "background-size": 'cover'});
+      parent.css({"background-image": 'url("' + d.thumbnail_url + '")', "background-size": 'cover'});
       parent.find(".news--feature__content").attr('href', insta);
       parent.find(".news--feature__name").html('<span class="news--feature__link">'+ '<svg height="45" width="46"><use xlink:href="#xu-instagram"></use></svg>' + d.author_name + '</span>');
-      parent.find(".news--feature__caption").html(d.title.slice(0, 75) + ( d.title.length > 75 ? "&hellip;" : ''));
     });
   }
 }

ae45091b241d95b0d1bceea1e9be30e1bfec4ac0
Greg McMullen <mcmullen.greg@gmail.com>
2020-10-30 15:00:32 -0400

    Ooh buddy; video play/pause button on BG videos

--- src/js/base.video.js
+++ src/js/base.video.js
@@ -1,10 +1,24 @@
 // Need to see if we are on a mobile device. If we aren't, let's post the video!
-if ( !detectmob() ){
-  var vid = $(".hero video");
-  if ( vid.length > 0 ){
-    vid.html('<source src="'+vid.data("bgvideo") + '" type="video/mp4" />');
-  }
+var vid = document.querySelector(".hero video");
+if ( !detectmob() && vid !== null ) { 
+  var button = document.createElement("button");
+  button.innerHTML = '<svg height="32" width="32" fill="white"><use xlink:href="#xu-pause"/></svg>';
+  button.style.cssText = "position: absolute; bottom: 3rem; right: 2rem; z-index: 100; padding: 1rem;";
+  button.className = "o-btn";
+  button.addEventListener('click', function(e){
+    if ( vid.paused ) {
+      vid.play();
+      button.innerHTML = '<svg height="32" width="32" fill="white"><use xlink:href="#xu-pause"/></svg>';
+    } else {
+      vid.pause();
+      button.innerHTML = '<svg height="32" width="32" fill="white"><use xlink:href="#xu-play"/></svg>';
+    }
+  });
+  vid.innerHTML = '<source src="'+vid.getAttribute('data-bgvideo')+'" type="video/mp4" />';
+  console.log(vid.parentNode);
+  vid.parentNode.appendChild(button);
 }
+
 // Video Button -- for things like Campus Life / Diversity
 $("#vid-btn").click(function(){
     $("#video-overlay iframe").attr('src', $(this).data('video'));

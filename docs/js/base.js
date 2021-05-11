a94067dde57a4754e0f9222b91e3463d6e9cba55
Greg McMullen <mcmullen.greg@gmail.com>
2021-05-11 09:59:57 -0400

    Updates to alerts

--- docs/js/base.js
+++ docs/js/base.js
@@ -685,6 +685,20 @@ window.onscroll = function(e) {
   }
   
 }
+const tables = document.querySelectorAll("table");
+tables.forEach(table => {
+  if ( table.classList.contains("no-resp")){
+  } else {
+    function wrap(top, selector, bottom){
+  var matches = document.querySelectorAll(selector);
+  for (var i = 0; i < matches.length; i++){
+    var modified = top + matches[i].outerHTML + bottom;
+    matches[i].outerHTML = modified;
+  }
+}
+wrap("<div style='display: grid;'><div class='resp-table'>", "table:not(.no-resp)", "</div></div>");
+   }; 
+});
 var Tabs = {
   init: function() {
     this.bindUIfunctions();

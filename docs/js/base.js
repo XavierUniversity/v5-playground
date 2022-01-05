2516611103277323df37bc80725250e8b013b994
Greg McMullen <mcmullen.greg@gmail.com>
2022-01-05 14:56:05 -0500

    Clean up some of the logic and time info

--- docs/js/base.js
+++ docs/js/base.js
@@ -28,17 +28,17 @@ if ( $("#announcements").length ){
       // Get difference from TODAY with Created Date
       var created = new Date(item.created);
       var today = new Date();
-      var millisBetween = today.getTime() - created.getTime();    
+      var millisBetween = today.getTime() - created.getTime();
       var days = Math.floor(millisBetween / (1000 * 60 * 60 * 24));
       var hours = Math.floor(millisBetween / (60 * 60 * 1000));
-      
+
       if ( item.audience.toLowerCase().indexOf(audience) >= 0 || audience == "all"){
         var ellapsed = ( hours <= 24 ? hours + " hour(s)" : days + " day(s)") + " ago";
-        announcement = 
+        announcement =
         '<button class="o-btn tabs__title accordion__btn" data-toggle="#'+item.id+'" role="tab">'
         +item.title+' - ' + ellapsed + '<svg class="icon accordion__icon accordion__icon--plus"><use xlink:href="#xu-plus"></use></svg><svg class="icon accordion__icon accordion__icon--minus"><use xlink:href="#xu-minus"></use></svg></button>';
         announcement += "<div class='accordion__content' id='"+item.id+"' role='tabpanel'>";
-        announcement += "<p>Posted: "+item.created+"</p>";
+        announcement += "<p>Posted: "+created.toLocaleTimeString('en-US', { hour: '1-digit', minute: '2-digit'})+"</p>";
         announcement += item.content;
         announcement += (item.link !== "" ? '<p><a href="' + item.link + '" title="Additional information for ' + item.title + '">Additional information</a></p>' : '' );
         announcement += "<p class='text--right'><a href='/announcements/flag?="+item.id +"' title='"+item.title+"'>Report announcement</a></p>";
@@ -47,9 +47,9 @@ if ( $("#announcements").length ){
       items.push(announcement);
     });
     if ( items.length >= 1 ){
-      $("#announcements").append(items); 
+      $("#announcements").append(items);
     } else {
-      $("#announcements").append('<div class="alert alert--info"><div class="alert--info__icon"><span class="fa" aria-hidden="true"></span></div><div class="alert__content"><p>There are no announcements at this time.</p></div></div>'); 
+      $("#announcements").append('<div class="alert alert--info"><div class="alert--info__icon"><span class="fa" aria-hidden="true"></span></div><div class="alert__content"><p>There are no announcements at this time.</p></div></div>');
     }
   });
 }
